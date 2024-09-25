0ebb8eadfa349942b3f4b659b13a7a689f2ed339
awchjimmy <awchjimmy@gmail.com>
2024-09-25 15:49:41 +0800

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -16,19 +16,25 @@ def load_credentials():
         login = content['login']
         password = content['password']
 
-def open_long():
-    start_chrome(server, headless=False)
-
-    # Click 'Accept'
-    wait_until(Button('Accept').exists)
-    click('Accept')
-
-    # Enter credentials
+def enter_credentials():
     load_credentials()
     wait_until(Button('Connect to account').exists)
     write(login, into='Enter Login')
     write(password, into='Enter Password')
     click('Connect to account')
+
+def click_accept():
+    wait_until(Button('Accept').exists)
+    click('Accept')
+
+def open_long():
+    start_chrome(server, headless=False)
+
+    # Click 'Accept'
+    click_accept()
+
+    # Enter credentials
+    enter_credentials()
 
 def close_long():
     pass
