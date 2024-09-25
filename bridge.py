7b7a5d3f12610746179f4176065962b426ea099a
awchjimmy <awchjimmy@gmail.com>
2024-09-25 16:58:37 +0800

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -22,10 +22,37 @@ def enter_credentials():
     write(login, into='Enter Login')
     write(password, into='Enter Password')
     click('Connect to account')
+    time.sleep(3)
 
 def click_accept():
     wait_until(Button('Accept').exists)
     click('Accept')
+
+def change_layout():
+    wait_until(Text('Symbol').exists)
+    time.sleep(2)
+    press(ALT + '3') # from 'CandleSticks' to 'Area'
+
+def enter_volume():
+    wait_until(Text('Symbol').exists)
+    press(F9) # Press F9 to Show Trade Form 
+    tfs = find_all(TextField())
+
+    if len(tfs) == 5:
+        input_comment = tfs[0]
+        input_stoploss = tfs[1]
+        input_takeprofit = tfs[2]
+        input_volume = tfs[3]
+        input_searchsymbol = tfs[4]
+
+        doubleclick(input_volume)
+        write('1', into=input_volume) # Volume: 1.00
+
+        click(Button('Buy by Market')) # Click 'Buy by Market'
+        wait_until(Button('OK').exists)
+    else:
+        print('Error: The layout has been changed.')
+
 
 def open_long():
     start_chrome(server, headless=False)
@@ -35,6 +62,15 @@ def open_long():
 
     # Enter credentials
     enter_credentials()
+
+    # Change layout
+    change_layout()
+
+    # Enter volume
+    enter_volume()
+
+    # Kill browser
+    kill_browser()
 
 def close_long():
     pass
