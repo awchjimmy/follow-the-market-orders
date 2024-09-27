0f271e6a621101abafb60542d270b31000171560
awchjimmy <awchjimmy@gmail.com>
2024-09-27 10:54:23 +0800

    fastapi

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import bridge
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-def main():
-    # bridge.open_long()
-    bridge.close_long()
+class TradingviewSignal(BaseModel):
+    action: str
+    contracts: str
+    marketPosition: str
+    positionSize: str
+    prevMarketPosition: str
+    price: str
+    symbol: str
+    time: str
 
-if __name__ == '__main__':
-    main()
+app = FastAPI()
+
+@app.post("/tradingview-signal/9781f744-9bc1-49da-9f11-75b654dd888f")
+def handle_signal(signal: TradingviewSignal):
+    if signal.action == 'buy':
+        bridge.open_long()
+    elif signal.action == 'sell':
+        bridge.close_long()
+    else:
+        print('Unknown action from tradingview.')
